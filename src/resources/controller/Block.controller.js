7f066276ea13345a249248c2e6f24c6dc80921ad
periscuelo <periscuelo@gmail.com>
2022-01-14 10:59:52 -0300

    Added error message customized

--- src/resources/controller/Block.controller.js
+++ src/resources/controller/Block.controller.js
@@ -1,4 +1,5 @@
 const { hash, readFile, writeOnFile } = require('../../utils')
+const { createErrorResponse } = require('../../utils/responses')
 
 const controller = {}
 
@@ -11,30 +12,30 @@ const _store = (req, res, receivedHash) => {
   const nextHash = hash('sha256', strToHash)
 
   readFile('./src/temp/nextchain.txt', (err, data) => {
-    if (err) res.code(500).send(err)
+    if (err) res.code(500).send(createErrorResponse('UNEXPECTED_ERROR', err))
     const lastHash = data.replace(/\r\n/g, '\n').split('\n').pop()
 
     // Validate chain to avoid store any hash not on sequence
     if (lastHash !== '' && lastHash === nextHash) {
-      return res.code(400).send('This hash already used')
+      return res.code(400).send(createErrorResponse('YADIW_ERROR', 'This hash already used'))
     } else if (lastHash !== '' && lastHash !== receivedHash) {
-      return res.code(400).send('You can\'t broke the chain. Use the correct hash to proceed!')
+      return res.code(400).send(createErrorResponse('YADIW_ERROR', 'You can\'t broke the chain. Use the correct hash to proceed!'))
     } else {
       writeOnFile('./src/temp/nextchain.txt', nextHash, nextHash, (errW) => {
         if (errW) {
-          return res.code(500).send(errW)
+          return res.code(500).send(createErrorResponse('UNEXPECTED_ERROR', errW))
         }
       })
       writeOnFile('./src/temp/nextnonce.txt', req.body.nonce, req.body.nonce, (errW) => {
         if (errW) {
-          return res.code(500).send(errW)
+          return res.code(500).send(createErrorResponse('UNEXPECTED_ERROR', errW))
         }
       })
     }
 
     writeOnFile('./src/temp/chains.csv', strToHash, receivedHash, (errW) => {
       if (errW) {
-        return res.code(500).send(errW)
+        return res.code(500).send(createErrorResponse('UNEXPECTED_ERROR', errW))
       } else {
         readFile('./src/temp/chains.csv', (errC, dataC) => {
           if (errC) return res.send(errC)
@@ -60,13 +61,13 @@ controller.storeChain = (req, res) => {
 
     // Validate parameters
     if (req.body.hash !== '0' && req.body.hash.length < 64) {
-      return res.code(400).send('Corrupted Hash!')
+      return res.code(400).send(createErrorResponse('YADIW_ERROR', 'Corrupted Hash!'))
     } else {
       receivedHash = req.body.hash.padStart(64, '0')
     }
 
     readFile('./src/temp/chains.csv', (err, data) => {
-      if (err) res.code(500).send(err)
+      if (err) res.code(500).send(createErrorResponse('UNEXPECTED_ERROR', err))
 
       const firstLook = data.replace(/\r\n/g, '\n').split('\n')
       const firstLine = firstLook.shift()
@@ -75,9 +76,9 @@ controller.storeChain = (req, res) => {
       if (firstLine !== '') {
         const csvData = firstLine.split(',')
         if (csvData[0] !== '0'.padStart(64, '0')) {
-          return res.code(422).send('This chain don\'t have the genesi line')
+          return res.code(422).send(createErrorResponse('SING_ERROR', 'This chain don\'t have the genesi line'))
         } else if (csvData[0] === receivedHash) {
-          return res.code(422).send('You can have only one genesi line')
+          return res.code(422).send(createErrorResponse('SING_ERROR', 'You can have only one genesi line'))
         }
 
         firstLook.unshift(firstLine)
@@ -87,9 +88,9 @@ controller.storeChain = (req, res) => {
           const csvData2 = value.split(',')
 
           if (csvData2[0] === receivedHash) {
-            return res.code(400).send('This hash already used')
+            return res.code(400).send(createErrorResponse('YADIW_ERROR', 'This hash already used'))
           } else if (parseInt(csvData2[2], 10) === req.body.nonce) {
-            return res.code(400).send('This nonce already used')
+            return res.code(400).send(createErrorResponse('YADIW_ERROR', 'This nonce already used'))
           }
 
           counter += 1
@@ -99,13 +100,13 @@ controller.storeChain = (req, res) => {
           }
         })
       } else if (req.body.hash !== '0' && firstLine === '') {
-        return res.code(422).send('This chain don\'t have the genesi line')
+        return res.code(422).send(createErrorResponse('SING_ERROR', 'This chain don\'t have the genesi line'))
       } else {
         _store(req, res, receivedHash)
       }
     })
   } catch (e) {
-    return res.code(422).send(e)
+    return res.code(422).send(createErrorResponse('SING_ERROR', e))
   }
 }
 
